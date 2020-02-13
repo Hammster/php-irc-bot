9d9e86748824868629ec607c446ef39ac63d0a98
Christian <60179743+christian-th1@users.noreply.github.com>
2020-02-13 19:51:53 -0300

    More channels instead of just one.
    Makes the bot able to respond to multiple channels.

--- bot.php
+++ bot.php
@@ -4,7 +4,7 @@
 set_time_limit(0);
 
 // Change these values!
-$channel  = '#openra';
+$channels  = array('#openra', '#another_channel', '#another_channel2');
 $nickname = 'HammsterBot';
 // $password = 'secret';
 $master   = 'Hammster';
@@ -18,8 +18,9 @@ fputs($socket, "NICK " . $nickname . "\n");
 fputs($socket, "USER " . $nickname . " 0 * :" . $master . "'s Bot\n");
 
 // Join channel
-fputs($socket, "JOIN " . $channel . "\n");
-
+foreach($channels as $channel) {
+	fputs($socket, "JOIN " . $channel . "\n");
+}
 // Force an endless while
 while (1) {
     // Continue the rest of the script here
@@ -42,14 +43,14 @@ while (1) {
                 chr(13)
             ), '', $ex[3]);
             if ($command == ":!alive?") {
-                fputs($socket, "PRIVMSG " . $channel . " :whazzup? \n");
+                fputs($socket, "PRIVMSG " . $ex[2] . " :whazzup? \n");
             }
             if ($command == ":!time") {
-                fputs($socket, "PRIVMSG " . $channel . " :" . date(DATE_RFC2822) . " \n");
+                fputs($socket, "PRIVMSG " . $ex[2] . " :" . date(DATE_RFC2822) . " \n");
             }
             if ($command == ":!help") {
-                fputs($socket, "PRIVMSG " . $channel . " :Hambot phpIRCbot v0.1 commands. \n");
-                fputs($socket, "PRIVMSG " . $channel . " :!alive?, !time, !slave, !chucknorris, !meme !meat \n");
+                fputs($socket, "PRIVMSG " . $ex[2] . " :Hambot phpIRCbot v0.1 commands. \n");
+                fputs($socket, "PRIVMSG " . $ex[2] . " :!alive?, !time, !slave, !chucknorris, !meme !meat \n");
             }
             if ($command == ":!slave") {
                 
@@ -57,26 +58,26 @@ while (1) {
                 $user  = substr($parts['0'], 1);
                 
                 if ($user == $master)
-                    fputs($socket, "PRIVMSG " . $channel . " :Yes master! \n");
+                    fputs($socket, "PRIVMSG " . $ex[2] . " :Yes master! \n");
                 else
-                    fputs($socket, "PRIVMSG " . $channel . " :get lost " . $user . " you filthy infidel! \n");
+                    fputs($socket, "PRIVMSG " . $ex[2] . " :get lost " . $user . " you filthy infidel! \n");
             }
             if ($command == ":!test") {
-                fputs($socket, "PRIVMSG " . $channel . " :value0 " . $ex[0] . ", value1 " . $ex[1] . ",value2 " . $ex[2] . ",value3 " . $ex[3] . "\n");
+                fputs($socket, "PRIVMSG " . $ex[2] . " :value0 " . $ex[0] . ", value1 " . $ex[1] . ",value2 " . $ex[2] . ",value3 " . $ex[3] . "\n");
             }
             if ($command == ":!chucknorris") {
                 $joke = json_decode(file_get_contents('http://api.icndb.com/jokes/random', true));
-                fputs($socket, "PRIVMSG " . $channel . " :" . $joke->value->joke . " \n");
+                fputs($socket, "PRIVMSG " . $ex[2] . " :" . $joke->value->joke . " \n");
             }
             if ($command == ":!meme") {
                 $meme = file_get_contents('http://api.automeme.net/text?lines=1');
-                fputs($socket, "PRIVMSG " . $channel . " :" . $meme . " \n");
+                fputs($socket, "PRIVMSG " . $ex[2] . " :" . $meme . " \n");
             }
             if ($command == ":!meat") {
                 $meat = file_get_contents('http://baconipsum.com/api/?type=all-meat&sentences=1');
                 $meat = explode(" ", $meat);
                 $meat = substr($meat['0'], 2);
-                fputs($socket, "PRIVMSG " . $channel . " :" . $meat . " \n");
+                fputs($socket, "PRIVMSG " . $ex[2] . " :" . $meat . " \n");
             }
         }
     }
